5777ccbccd16654464ccf706f368fdd589c1e77a
Vasily Leonenko <vasiliy.leonenko@gmail.com>
2025-06-14 22:06:25 +0300

    [release-branch.go1.22] cmd/compile: update arm64 duffcopy to use SIMD registers and instructions
    Raspberry Pi 5 (Cortex-A76)
    goos: linux
    goarch: arm64
    pkg: runtime
                 │ loweredmove.log │              duffcopy.log              │
                 │     sec/op      │    sec/op     vs base                  │
    CopyFat/7          1.046n ± 0%    1.046n ± 0%        ~ (p=0.474 n=10)
    CopyFat/8         0.8342n ± 0%   0.8342n ± 0%        ~ (p=0.232 n=10)
    CopyFat/11         1.046n ± 0%    1.046n ± 0%   +0.05% (p=0.022 n=10)
    CopyFat/12         1.044n ± 0%    1.044n ± 0%        ~ (p=1.000 n=10) ¹
    CopyFat/13         1.046n ± 0%    1.046n ± 0%        ~ (p=0.474 n=10)
    CopyFat/14         1.046n ± 0%    1.046n ± 0%        ~ (p=1.000 n=10)
    CopyFat/15         1.045n ± 0%    1.046n ± 0%   +0.10% (p=0.011 n=10)
    CopyFat/16         1.251n ± 0%    1.251n ± 0%        ~ (p=1.000 n=10)
    CopyFat/24         2.796n ± 0%    2.794n ± 0%   -0.07% (p=0.000 n=10)
    CopyFat/32         2.759n ± 0%    2.751n ± 0%   -0.29% (p=0.000 n=10)
    CopyFat/64         2.707n ± 0%    2.493n ± 1%   -7.89% (p=0.000 n=10)
    CopyFat/72         2.648n ± 1%    2.671n ± 0%   +0.87% (p=0.002 n=10)
    CopyFat/128        5.115n ± 0%    4.942n ± 0%   -3.39% (p=0.000 n=10)
    CopyFat/256        9.723n ± 0%    8.700n ± 0%  -10.53% (p=0.000 n=10)
    CopyFat/512        19.48n ± 0%    17.09n ± 0%  -12.29% (p=0.000 n=10)
    CopyFat/520        20.05n ± 0%    17.03n ± 0%  -15.06% (p=0.000 n=10)
    CopyFat/1024       39.51n ± 0%    32.86n ± 0%  -16.83% (p=0.000 n=10)
    CopyFat/1032       39.55n ± 0%    31.59n ± 0%  -20.13% (p=0.000 n=10)
    CopyFat/1040       39.56n ± 0%    31.99n ± 0%  -19.14% (p=0.000 n=10)
    CopyFat/2040       54.36n ± 0%    54.35n ± 0%        ~ (p=0.188 n=10)
    CopyFat/2048       54.41n ± 0%    54.34n ± 0%   -0.13% (p=0.019 n=10)
    CopyFat/2056       55.05n ± 1%    55.21n ± 0%   +0.30% (p=0.027 n=10)
    geomean            5.432n         5.158n        -5.03%
    
    Change-Id: I843ccde4535ad67dd1dea75fa316ca72b999cd9f

--- src/runtime/mkduff.go
+++ src/runtime/mkduff.go
@@ -169,9 +169,9 @@ func copyARM64(w io.Writer) {
 	// R20 and R21 are updated as a side effect
 	fmt.Fprintln(w, "TEXT runtime·duffcopy<ABIInternal>(SB), NOSPLIT|NOFRAME, $0-0")
 
-	for i := 0; i < 64; i++ {
-		fmt.Fprintln(w, "\tLDP.P\t16(R20), (R26, R27)")
-		fmt.Fprintln(w, "\tSTP.P\t(R26, R27), 16(R21)")
+	for i := 0; i < 32; i++ {
+		fmt.Fprintln(w, "\tFLDPQ.P\t32(R20), (F16, F17)")
+		fmt.Fprintln(w, "\tFSTPQ.P\t(F16, F17), 32(R21)")
 		fmt.Fprintln(w)
 	}
 	fmt.Fprintln(w, "\tRET")
