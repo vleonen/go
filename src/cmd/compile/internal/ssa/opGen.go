70effbee10bc2504193c244d32b609ce61bedd58
Vasily Leonenko <vasiliy.leonenko@gmail.com>
2025-06-14 22:38:31 +0300

    [release-branch.go1.22] cmd/compile: update arm64 LoweredZero to use SIMD registers and instructions
    Raspberry Pi 5 (Cortex-A76)
    goos: linux
    goarch: arm64
    pkg: runtime
                  │   base.log   │            loweredzero.log             │
                  │    sec/op    │    sec/op     vs base                  │
    ClearFat/7      0.6271n ± 0%   0.6271n ± 0%        ~ (p=0.563 n=10)
    ClearFat/8      0.5801n ± 0%   0.5803n ± 0%        ~ (p=0.591 n=10)
    ClearFat/11     0.6271n ± 0%   0.6271n ± 0%        ~ (p=0.520 n=10)
    ClearFat/12     0.6267n ± 0%   0.6267n ± 0%        ~ (p=1.000 n=10)
    ClearFat/13     0.6271n ± 0%   0.6271n ± 0%    0.00% (p=0.033 n=10)
    ClearFat/14     0.6271n ± 0%   0.6271n ± 0%        ~ (p=0.474 n=10)
    ClearFat/15     0.6271n ± 0%   0.6271n ± 0%        ~ (p=1.000 n=10)
    ClearFat/16      1.251n ± 0%    1.251n ± 0%        ~ (p=1.000 n=10)
    ClearFat/24      1.950n ± 0%    2.502n ± 0%  +28.31% (p=0.000 n=10)
    ClearFat/32      2.840n ± 0%    2.841n ± 0%        ~ (p=0.512 n=10)
    ClearFat/40      2.502n ± 0%    2.502n ± 0%        ~ (p=1.000 n=10)
    ClearFat/48      2.597n ± 0%    2.502n ± 0%   -3.64% (p=0.000 n=10)
    ClearFat/56      2.626n ± 0%    2.626n ± 0%        ~ (p=1.000 n=10)
    ClearFat/64      2.627n ± 0%    2.627n ± 0%        ~ (p=1.000 n=10)
    ClearFat/72      2.628n ± 0%    2.628n ± 0%        ~ (p=1.000 n=10) ¹
    ClearFat/128     3.767n ± 0%    3.767n ± 0%        ~ (p=1.000 n=10) ¹
    ClearFat/256     7.093n ± 0%    7.093n ± 0%        ~ (p=1.000 n=10)
    ClearFat/512     13.77n ± 0%    13.77n ± 0%        ~ (p=1.000 n=10) ¹
    ClearFat/520     14.18n ± 0%    14.18n ± 0%        ~ (p=1.000 n=10) ¹
    ClearFat/1024    27.82n ± 0%    27.82n ± 0%        ~ (p=1.000 n=10)
    ClearFat/1032    28.04n ± 0%    28.04n ± 0%        ~ (p=1.000 n=10) ¹
    ClearFat/1040    34.19n ± 0%    28.19n ± 0%  -17.55% (p=0.000 n=10)
    ClearFat/2040    61.54n ± 3%    54.65n ± 0%  -11.20% (p=0.000 n=10)
    ClearFat/2048    68.57n ± 0%    55.11n ± 0%  -19.63% (p=0.000 n=10)
    ClearFat/2056    61.06n ± 1%    55.08n ± 0%   -9.79% (p=0.000 n=10)
    geomean          3.986n         3.920n        -1.67%
    
    Change-Id: I1e924b51277a13852791cb404c17989dc4621281

--- src/cmd/compile/internal/ssa/opGen.go
+++ src/cmd/compile/internal/ssa/opGen.go
@@ -22616,7 +22616,7 @@ var opcodeTable = [...]opInfo{
 				{0, 65536},     // R16
 				{1, 670826495}, // R0 R1 R2 R3 R4 R5 R6 R7 R8 R9 R10 R11 R12 R13 R14 R15 R16 R17 R19 R20 R21 R22 R23 R24 R25 R26 R30
 			},
-			clobbers: 65536, // R16
+			clobbers: 2147549184, // R16 F0
 		},
 	},
 	{
